44a6644a9c3452efe4e42e1e78dbe44cd54f02fe
kzw3933 <3099097649@qq.com>
2023-03-25 22:50:53 +0800

    学习使用android服务的使用以及多线程编程并实现下载功能

---
+++ app/src/main/java/com/example/uapp/DownloadListener.java
@@ -0,0 +1,10 @@
+package com.example.uapp;
+
+public interface DownloadListener {
+
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
--- app/src/main/java/com/example/uapp/MainActivity.java
+++ app/src/main/java/com/example/uapp/MainActivity.java
@@ -2,60 +2,91 @@ package com.example.uapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
-import android.provider.ContactsContract;
+import android.os.IBinder;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DownloadService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            downloadBinder = (DownloadService.DownloadBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button sendNotice = (Button) findViewById(R.id.send_notice);
-        sendNotice.setOnClickListener(this);
+        Button startDownload = (Button) findViewById(R.id.start_download);
+        Button pauseDownload = (Button) findViewById(R.id.pause_download);
+        Button cancelDownload = (Button) findViewById(R.id.cancel_download);
+        Intent intent = new Intent(this, DownloadService.class);
+        startService(intent);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] { android.Manifest.permission.WRITE_EXTERNAL_STORAGE }, 1);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.send_notice:
-                Intent intent = new Intent(this, NotificationActivity.class);
-                PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-                Notification notification = new NotificationCompat.Builder(this)
-                        .setContentTitle("This is content title")
-                        .setContentText("This is content text")
-                        .setWhen(System.currentTimeMillis())
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                        .setContentIntent(pi)
-                        .build();
-                manager.notify(1, notification);
+        if(downloadBinder == null) {
+            return ;
+        }
+        switch (v.getId()) {
+            case R.id.start_download:
+                String url = "https://raw.githubusercontent.com/guolindev/eclipse/master/eclipse-inst-win64.exe";
+                downloadBinder.startDownload(url);
+                break;
+            case R.id.pause_download:
+                downloadBinder.pauseDownload();
+                break;
+            case R.id.cancel_download:
+                downloadBinder.cancelDownload();
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "拒绝权限无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(connection);
     }
 
 }
