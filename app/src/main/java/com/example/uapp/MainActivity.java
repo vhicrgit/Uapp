f3f94a1868bc4495ae4cc8becba0e0ec436f79e8
kzw3933 <3099097649@qq.com>
2023-03-25 15:19:34 +0800

    学习android上的文件存储

--- app/src/main/java/com/example/uapp/MainActivity.java
+++ app/src/main/java/com/example/uapp/MainActivity.java
@@ -2,31 +2,87 @@ package com.example.uapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.text.TextUtils;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class MainActivity extends BaseActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button forceButton = (Button) findViewById(R.id.force_offline);
-        forceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("com.example.uapp.FORCE_OFFLINE");
-                sendBroadcast(intent);
+
+        editText = (EditText) findViewById(R.id.edit);
+        String inputText = load();
+        if(!TextUtils.isEmpty(inputText)) {
+            editText.setText(inputText);
+            editText.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = editText.getText().toString();
+        save(inputText);
+    }
+
+    public void save(String inputText) {
+        FileOutputStream fout = null;
+        BufferedWriter writer = null;
+        try {
+            fout = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(fout));
+            writer.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+        }
+    }
+
+    public String load() {
+        FileInputStream fin = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            fin = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(fin));
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
